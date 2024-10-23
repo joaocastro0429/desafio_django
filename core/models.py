62b4516539d90bab653b7eef81eed4af4e4e3ead
joaocastro <joaocastro0429@gmail.com>
2024-10-22 21:25:22 -0300

    criando desafio imersão fullcycle

---
+++ core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag, related_name='posts')  # O related_name é importante
+
+    def __str__(self):
+        return self.title
---
+++ core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+
+
+def blog_list(request):
+    posts=Post.objects.all()
+    return render(request, 'blog_list.html',{'posts':posts})
